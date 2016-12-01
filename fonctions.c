4ef46b4facae4c6273844ef18a5f4372b655c104
EliseRMC <elise.ramouche@outlook.fr>
2016-12-01 15:38:15 +0100

    Génération fichier OK ; nb positions ambigues OK

--- fonctions.c
+++ fonctions.c
@@ -1,5 +1,3 @@
-#include "structures.h"
-
 // ** Fonctions appelées lors de la génération de génotypes aléatoires **
 
 //Fonction tirage aleatoire dans l'intervalle [a,b[
@@ -8,111 +6,174 @@ int rand_a_b(int a, int b)
 	return rand()%(b-a) +a;
 }
 
-//Fonction génération de loci aléatoire avec positions ambigues
-void tirage_avec_ambiguite(int* genotype, int* haplotype1, int* haplotype2, int position, int nbLociAmbigus)
-{
-	int lociAleatoire, lociAmbHap=0;
-	
-	lociAleatoire = rand_a_b(0,3);
-	
-	if (lociAleatoire == 1 || lociAleatoire == 0)
-	{
-		genotype[position] = lociAleatoire;
-		haplotype1[position] = lociAleatoire;
-		haplotype2[position] = lociAleatoire;
-	}			
-	else if (lociAleatoire == 2)//Si un loci ambigu dans la séquence, sélection aléatoire du 0 ou du 1 pour les haplotypes générés
-	{ 
-		genotype[position] = lociAleatoire;
-		lociAmbHap = rand_a_b(0, 2) ; 
-		haplotype1[position] = lociAmbHap;
-		if (lociAmbHap == 0) 
-		{
-			haplotype2[position] = 1;
-		}else if (lociAmbHap == 1)
-		{
-			haplotype2[position] = 0;
-		}
-		nbLociAmbigus ++ ;
-	} 
-}
-
-//Fonction génération de loci aléatoire sans positions ambigues
-void tirage_sans_ambiguite(int* genotype, int* haplotype1, int* haplotype2, int position)
-{
-	int lociAleatoire=0;
-	lociAleatoire = rand_a_b(0, 2) ; //Tirage aléatoire dans l'intervalle [0, 2[
-	if (lociAleatoire == 1 || lociAleatoire == 0)
-	{
-		genotype[position] = lociAleatoire ;
-		haplotype1[position] = lociAleatoire;
-		haplotype2 [position] = lociAleatoire;
-	}
-}
-
-//Fonction génération aléatoire des positions d'un génome
-void tirage_loci_aleatoire(int* genotype, int* haplotype1, int* haplotype2, int tailleGenotype, int maxLociAmbigus)
-{
-	int nbLociAmbigus=0;
-	int position = 1;
-	
-	for (position = 0; position < tailleGenotype; position ++)
-	{
-		if (nbLociAmbigus < maxLociAmbigus)
-		{
-			tirage_avec_ambiguite(genotype, haplotype1, haplotype2, position, nbLociAmbigus);
-		}
-		else if (nbLociAmbigus >= maxLociAmbigus)
-		{
-			tirage_sans_ambiguite(genotype, haplotype1, haplotype2, position);
-		}
-	}
-}
-
-//Fonction génération aléatoire d'un génotypes pour chacun des individus	
-void generation_genotype_aleatoire(int nbIndividus, int tailleGenotype, int maxLociAmbigus)
-{
-	//Initialisation des tableaux de génotype et d'haplotypes
-	int* genotype = NULL;
-	int* haplotype1 = NULL;
-	int* haplotype2 = NULL;
-	
-	//Allocations de mémoire
-	genotype = malloc(tailleGenotype * sizeof(int));
-	haplotype1 = malloc(tailleGenotype * sizeof(int));
-	haplotype2 = malloc(tailleGenotype * sizeof(int));
-	
-	//Initialisation compteur du nombre d'individus
-	int individu=1;
-	
-	//Boucle de génération aléatoire de génotype et d'haplotypes pour chacun des individus
-	for (individu = 1; individu <= nbIndividus; individu ++)
-	{
-		tirage_loci_aleatoire(genotype, haplotype1, haplotype2, tailleGenotype, maxLociAmbigus);
-		
-		//Affichages du génotype et des haplotypes générés pour chaque individu
-		printf("\n\nGenotype Individu %d : ", individu);
-		affiche_tableau(genotype, tailleGenotype);
-		printf("\nHaplotype 1  : ");
-		affiche_tableau(haplotype1, tailleGenotype);
-		printf("\nHaplotype 2 : ");
-		affiche_tableau(haplotype2, tailleGenotype);
-		
-		creer_fichier_genotypes(genotype, tailleGenotype, individu);
-		creer_fichier_haplotypes(genotype, haplotype1, haplotype2, tailleGenotype, individu);
-	}
-}
 
 //Fonction affichage contenu de tableau
 void affiche_tableau(int tableau[], int tailleTableau)
 {
 	int i;
-	
+
 	for (i = 0 ; i < tailleTableau ; i++)
 	{
 		printf("%d", tableau[i]);
 	}
 
+}
+
+
+//Fonction génération aléatoire des positions d'un génome
+void tirage_loci_aleatoire(int* genotype, int tailleGenotype)
+{
+	int position;
+	int lociAleatoire=0;
+
+	for (position = 0; position < tailleGenotype; position ++)
+	{
+
+	lociAleatoire = rand_a_b(0, 2) ; //Tirage aléatoire dans l'intervalle [0, 2[
+    genotype[position] = lociAleatoire ;
+
+	}
+}
+
+//Fonction génération aléatoire des positions d'un génome
+void tirage_loci_ambigu(int* genotype, int tailleGenotype, int maxLociAmbigus)
+{
+	int i;
+    int* pos_locus_ambigu = NULL;
+	int* deja_fait = NULL;
+
+	//afficherVect(locus, taille_locus);
+	pos_locus_ambigu = malloc((maxLociAmbigus) * sizeof (int));
+	deja_fait = malloc((tailleGenotype) * sizeof (int));
+
+    for(i = 0; i < maxLociAmbigus; i++){
+        do{
+            pos_locus_ambigu[i] = rand_a_b(0, tailleGenotype) ;
+        }while(deja_fait[pos_locus_ambigu[i]] == 1);
+
+        deja_fait[pos_locus_ambigu[i]]  = 1;
+        genotype[pos_locus_ambigu[i]] = 2;
+        //afficherVect(locus, taille_locus);
+    }
+    //free(pos_locus_ambigu); //BUG à la compil
+    //free(deja_fait);
+}
+
+void tirage_haplotypes(int* genotype, int* haplotype1, int* haplotype2, int tailleGenotype)
+{
+    int position;
+    int lociAmbHap;
+
+    for (position = 0; position < tailleGenotype; position ++)
+    {
+        lociAmbHap=0;
+
+        haplotype1[position] = genotype[position];
+        haplotype2[position] = genotype[position];
+
+		if(genotype[position]==2){
+
+            lociAmbHap = rand_a_b(0, 2) ;
+            haplotype1[position] = lociAmbHap;
+            if (lociAmbHap == 0) { haplotype2[position] = 1;
+            }else//(lociAmbHap == 1)
+            { haplotype2[position] = 0;}
+		}
+    }
+}
+
+void nouveaux_tirages_si_doublon(int* genotype1, int* genotype2, int* haplotype1, int* haplotype2, int tailleGenotype, int maxLociAmbigus){
+
+    int similaire = 0;
+    int i;
+
+    //1ere verification de similarité entre genotype
+    for(i = 0; i < tailleGenotype; i++){
+
+        if (genotype1[i] == genotype2[i]) similaire++;
+    }
+
+    if(similaire == tailleGenotype){
+
+        do{//nouveau tirage s'il sont similaires
+            similaire = 0;
+
+            tirage_loci_aleatoire(genotype2, tailleGenotype);
+            tirage_loci_ambigu(genotype2, tailleGenotype, maxLociAmbigus);
+            tirage_haplotypes(genotype2, haplotype1, haplotype2, tailleGenotype);
+
+            for(i = 0; i < tailleGenotype; i++){
+
+                if (genotype1[i] == genotype2[i]) similaire++;
+            }
+
+        }while(similaire == tailleGenotype); //on prévoit le cas d'un troisième (ou plus!) tirage similaire
+      }
+    //affiche_tableau(genotype1, tailleGenotype);
+    //affiche_tableau(genotype2, tailleGenotype);
+}
+
+
+//Fonction génération aléatoire d'un génotypes pour chacun des individus
+void generation_genotype_aleatoire(int nbIndividus, int tailleGenotype, int maxLociAmbigus)
+{
+	//Initialisation des tableaux de génotype et d'haplotypes
+	int** liste_geno = NULL;
+	int** liste_haplo1 = NULL;
+	int** liste_haplo2 = NULL;
+
+	//Allocations de mémoire
+	liste_geno = (int**)malloc((nbIndividus) * sizeof (int*));
+	liste_haplo1 = (int**)malloc((nbIndividus) * sizeof (int*));
+	liste_haplo2 = (int**)malloc((nbIndividus) * sizeof (int*));
+
+	//Initialisation compteur du nombre d'individus
+	int individu, individu2;
+
+	//Boucle de génération aléatoire de génotype et d'haplotypes pour chacun des individus
+	for (individu = 0; individu < nbIndividus; individu ++)
+	{
+
+        liste_geno[individu] = (int*)malloc(tailleGenotype * sizeof(int));
+        liste_haplo1[individu] = (int*)malloc(tailleGenotype * sizeof(int));
+        liste_haplo2[individu] = (int*)malloc(tailleGenotype * sizeof(int));
+
+	    tirage_loci_aleatoire(liste_geno[individu], tailleGenotype);
+        tirage_loci_ambigu(liste_geno[individu], tailleGenotype, maxLociAmbigus);
+        tirage_haplotypes(liste_geno[individu], liste_haplo1[individu], liste_haplo2[individu], tailleGenotype);
+
+
+		//Affichages du génotype et des haplotypes générés pour chaque individu
+		printf("\n\n===Individu %d ===", individu+1);
+		printf("\nGenotype    : ");
+		affiche_tableau(liste_geno[individu], tailleGenotype);
+		printf("\nHaplotype 1 : ");
+		affiche_tableau(liste_haplo1[individu], tailleGenotype);
+		printf("\nHaplotype 2 : ");
+		affiche_tableau(liste_haplo2[individu], tailleGenotype);
+		
+		creer_fichier_genotypes(liste_geno[individu], tailleGenotype, individu);
+		creer_fichier_haplotypes(liste_geno[individu], liste_haplo1[individu], liste_haplo2[individu], tailleGenotype, individu);
+	}
+/*
+	for (individu = 0; individu < nbIndividus; individu ++){
+        for(individu2 = 0; individu2 < nbIndividus; individu2++){
+        //possibilite d optimiser pour eviter de comparer liste_geno[i] avec lui meme
+        nouveaux_tirages_si_doublon(liste_geno[individu], liste_geno[individu2], liste_haplo1[individu2], liste_haplo1[individu2],tailleGenotype, maxLociAmbigus);
+        //complexite de type taille_locus^3 (3eme boucle for dans la fct)
+        //possibilite d optimiser en evitant une comparaison 2 a 2 mais plutot de type "diviser pour regner" ?
+	    }
+	}*/
+
+	//libération de mémoire alloué dynamiquement
+	for (individu = 1; individu <= nbIndividus; individu ++){
+           free(liste_geno[individu]);
+           free(liste_haplo1[individu]);
+           free(liste_haplo2[individu]);
+	}
+	free(liste_geno);
+	free(liste_haplo1);
+	free(liste_haplo2);
 }
 
 //Fonction écriture fichiers texte
@@ -168,304 +229,3 @@ void creer_fichier_haplotypes(int genotype[], int haplotype1[], int haplotype2[]
 	}
 }
 			
-	
-	
-
-
-
-
-
-
-
-
-
-
-// ** Fonctions appelées lors de l'exécution de l'algorithme d'inférence d'haplotypes **
-
-//Alias
-
-/*struct TypeChaine80{
-    char chaineCaratere[80];
-};
-
-////////////////////////////////////////////////
-
-typedef struct TCell{
-    int info;
-    TPtr suiv;
-};
-
-
-////////////////////////////////////////////////
-
-void afficherErreur(char typeErreur){
-    printf("Erreur: ");
-    switch(typeErreur){
-        case 'L':
-            printf("liste vide.\n");
-            break;
-        case 'P':
-            printf("position erronee.\n");
-            break;
-        case 'V':
-            printf("valeur inexistante.\n");
-        break;
-        case 'F':
-            printf("ouverture du fichier impossible.\n");
-        break;
-    }
-}
-
-////////////////////////////////////////////////
-
-TPtr *creationElem(int val){
-    TPtr cell = (TCell*) malloc(sizeof(TCell));
-    cell -> info = val;
-
-    //verification de reussite
-    if(cell == NULL){
-    exit(1);
-    }
-
-    return cell;
-}
-
-////////////////////////////////////////////////
-
-void afficherListe(TPtr *adr_liste){
-
-    //verification d'existence de liste
-    if (adr_liste == NULL)
-    {
-        afficherErreur('L');
-        return;
-    }
-
-    TPtr p = *adr_liste;
-
-    //afficher chaque cellule
-    while (p != NULL)
-    {
-        printf("%d -> ", p->info);
-        p = p -> suiv;
-    }
-    printf("NULL\n"); //marquer la fin de liste
-}
-
-////////////////////////////////////////////////
-
-void insDebut(TPtr *adr_liste, int val, char bool_affiche){
-
-    //verification d'existence de liste
-    if (adr_liste == NULL)
-    {
-        afficherErreur('L');
-        return;
-    }
-
-    // creation du nouvel element
-    TPtr p_nouv;
-    p_nouv=creationElem(val);
-    // insertion de l'element au debut de la liste
-    p_nouv -> suiv = *adr_liste;
-    *adr_liste = p_nouv;
-
-    if (bool_affiche=='T') afficherListe(adr_liste);
-}
-
-////////////////////////////////////////////////
-
-void insPos(TPtr *adr_liste, int val, int k, char bool_affiche){
-
-    //verification d'existence de liste
-    if (adr_liste == NULL)
-    {
-        afficherErreur('L');
-        return;
-    }
-
-    //variables
-    TPtr prec = NULL, p = *adr_liste;
-    int i = 1;
-
-    //placement a la bonne position
-    while(p != NULL && i < k){
-        prec = p;
-        p = p -> suiv;
-        i++;
-    }
-    if(k==1){ //insertion en debut de liste (cas de liste vide [p == NULL] ET de liste preexistante)
-        insDebut(adr_liste, val, bool_affiche);
-        return;
-        }//sinon k>1
-    if (p == NULL && k!= i){//k > i (k>longueur de la liste)
-            afficherErreur('P');
-            return;
-    }//sinon insertion en milieu de liste OU en fin de liste (p == NULL);
-    insDebut(&prec -> suiv, val, bool_affiche);
-}
-
-////////////////////////////////////////////////
-
-void supprDebut(TPtr *adr_liste, char bool_affiche){
-
-    //verification d'existence de liste
-    if (adr_liste == NULL)
-    {
-        afficherErreur('L');
-        return;
-    }
-
-    //raccordements
-    TPtr p = *adr_liste;
-    *adr_liste = p -> suiv;
-
-    free(p); //supression
-
-    if (bool_affiche=='T') afficherListe(adr_liste);
-}
-
-////////////////////////////////////////////////
-
-void supprPos(TPtr *adr_liste, int k, char bool_affiche){
-
-    //verification d'existence de liste
-    if (adr_liste == NULL)
-    {
-        afficherErreur('L');
-        return;
-    }
-
-    //variables
-    TPtr prec = NULL, p = *adr_liste;
-    int i = 1;
-
-    //placement a la bonne position
-    while(p != NULL && i < k){
-        prec = p;
-        p = p -> suiv;
-        i++;
-    }
-    if(k==1){ //insertion en debut de liste (cas de liste vide ET de liste preexistante)
-        supprDebut(adr_liste, bool_affiche);
-        return;
-        }//sinon k>1
-    if (p == NULL && k!= i){//k > i (k>longueur de la liste)
-            afficherErreur('P');
-            return;
-    }//sinon insertion en milieu de liste OU en fin de liste (p == NULL);
-    supprDebut(&prec -> suiv, bool_affiche);
-}
-
-////////////////////////////////////////////////
-
-void accesVal(TPtr *adr_liste, int val, TPtr *adr_ptrCell){
-
-    //variables
-    TPtr p = *adr_liste;
-
-    while(p != NULL){
-        if(p -> info == val){//cas du match
-        printf("%d\n", p->info);
-        adr_ptrCell = p; return;
-        }
-        p = p -> suiv; //sinon incrementation
-    }afficherErreur('V');
-    *adr_ptrCell = NULL; // si on ne trouve pas
-}
-
-////////////////////////////////////////////////
-
-void insertListeTriee(TPtr *adr_liste, int val, char bool_affiche){
-
-    //verification d'existence de liste
-    if (adr_liste == NULL)
-    {
-        afficherErreur('L');
-        return;
-    }
-
-    //variables
-    TPtr p = *adr_liste; TPtr prec = NULL;
-
-    //placement a la bonne position
-    while(p != NULL && val > p-> info){
-        prec = p;
-        p = p -> suiv;
-    }//si liste vide, insertion en tete
-    if (prec == NULL){
-        insDebut(adr_liste, val, bool_affiche);
-        return;
-    }//sinon insertion au milieu OU en fin de liste
-    insDebut(&prec -> suiv, val, bool_affiche);
-}
-
-////////////////////////////////////////////////
-
-void lireFichier(char nomDuFichier[], char modeOuverture[]){
-
-    FILE* fichier = NULL;
-    fichier = fopen(nomDuFichier, modeOuverture);
-
-    if (fichier == NULL)
-    {
-        afficherErreur('F');;
-    }//sinon
-    char chaine[50] = "";
-
-    while (fgets(chaine, 50, fichier) != NULL)
-    {
-        printf("%s", chaine);
-    }
-
-        fclose(fichier);
-}
-
-////////////////////////////////////////////////
-
-int longueurVecteur(int v[]){
-    int i;
-    for (i=0; i+)
-}
-int triRapide(int v[], int g, int d){
-
-    //variables locales
-    int ind_pivot;
-
-    //affectation
-    g = v[i];
-    d =
-
-    if(g < d){//sinon vecteur vide
-        ind_pivot=partitionner(v, g, d);
-        triRapide(v, g, ind_pivot - 1);
-        triRapide(v, ind_pivot + 1, d);
-    }
-    return v[];
-}
-
-int partitionner(int v[], int g, int d){
-
-    //variables locales
-    int pivot, i, j;
-
-    //affectation
-    pivot=v[g];
-    i=g+1;
-    j=d;
-
-    while(i <= j){
-        if(v[i] <= pivot){
-            i++;
-        }//sinon
-        while(v[j] > pivot){
-            j--;
-        }
-        if(i < j){
-        echanger(v,i,j);
-        i++; j--;
-    }
-    echanger(v,g,j);
-    return j;
-}*/
-
--- structures.h
+++ structures.h
@@ -7,10 +7,13 @@ void affiche_tableau(int tableau[], int tailleTableau);
 
 void tirage_avec_ambiguite(int* genotype, int* haplotype1, int* haplotype2, int position, int nbLociAmbigus);
 void tirage_sans_ambiguite(int* genotype, int* haplotype1, int* haplotype2, int position);
-void tirage_loci_aleatoire(int* genotype, int* haplotype1, int* haplotype2, int tailleGenotype, int maxLociAmbigus);
-void generation_genotype_aleatoire(int nbIndividus, int tailleGenotype, int maxLociAleatoire);
+void tirage_loci_aleatoire(int* genotype, int tailleGenotype);
+void generation_genotype_aleatoire(int nbIndividus, int tailleGenotype, int maxLociAmbigus);
 void creer_fichier_genotypes(int genotype[], int tailleTableau, int individu);
 void creer_fichier_haplotypes(int genotype[], int haplotype1[], int haplotype2[], int tailleTableau, int individu);
+void tirage_loci_ambigu(int* genotype, int tailleGenotype, int maxLociAmbigus);
+void tirage_haplotypes(int* genotype, int* haplotype1, int* haplotype2, int tailleGenotype);
+void nouveaux_tirages_si_doublon(int* genotype1, int* genotype2, int* haplotype1, int* haplotype2, int tailleGenotype, int maxLociAmbigus)
 
 
 // ** Definitions Inference haplotypes **
